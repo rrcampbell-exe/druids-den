41f82365aea072971283111927c2348cb5725961
rrcampbell-exe <campbell.ryan.r@gmail.com>
2025-06-03 07:22:25 -0500

    fix: update styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import './App.css'
 const App = () =>
   <div className='landing-page'>
     <h1 className='landing-title'>
-      <span className='the'>&#xE01B;&#xE027;&#xE033;</span>
-      <br/>
-      <span className='druids-den'>&#xE01F;&#xE026;&#xE03D;&#xE005;&#xE01F;&#xE03B; &#xE01F;&#xE033;&#xE021;</span>
-      <br/>
-      <span className='three-rays'>&#xE000;</span>
+      <p className='the'>&#xE01B;&#xE027;&#xE033;</p>
+      <p className='druids-den'>&#xE01F;&#xE026;&#xE03D;&#xE005;&#xE01F;&#xE03B; &#xE01F;&#xE033;&#xE021;</p>
+      <p className='three-rays'>&#xE000;</p>
     </h1>
     <p>coming soon</p>
   </div>
