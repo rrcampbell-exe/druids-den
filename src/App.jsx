431eff0f043f128cb9cefd576b7546358f6967a5
rrcampbell-exe <campbell.ryan.r@gmail.com>
2025-06-02 20:12:32 -0500

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () =>
+  <div className='coelbren'>
+    <h2>&#xE01B;&#xE027;&#xE033;</h2>
+    <h1>&#xE01F;&#xE026;&#xE03D;&#xE005;&#xE01F;&#xE03B; &#xE01F;&#xE033;&#xE021;</h1>
+    <h3>&#xE000;</h3>
+    <p>coming soon</p>
+  </div>
+
+export default App
