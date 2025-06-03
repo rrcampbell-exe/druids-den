a106daac7a5edc2bba63de9e7de5a5c5090e4f7e
rrcampbell-exe <campbell.ryan.r@gmail.com>
2025-06-03 07:02:05 -0500

    fix: update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 
 const App = () =>
-  <div className='coelbren'>
-    <h2>&#xE01B;&#xE027;&#xE033;</h2>
-    <h1>&#xE01F;&#xE026;&#xE03D;&#xE005;&#xE01F;&#xE03B; &#xE01F;&#xE033;&#xE021;</h1>
-    <h3>&#xE000;</h3>
+  <div className='landing-page'>
+    <h1 className='landing-title'>
+      <span className='the'>&#xE01B;&#xE027;&#xE033;</span>
+      <br/>
+      <span className='druids-den'>&#xE01F;&#xE026;&#xE03D;&#xE005;&#xE01F;&#xE03B; &#xE01F;&#xE033;&#xE021;</span>
+      <br/>
+      <span className='three-rays'>&#xE000;</span>
+    </h1>
     <p>coming soon</p>
   </div>
 
