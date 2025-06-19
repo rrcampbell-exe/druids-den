3720de3a8c9a82dde0bec111fbcdf97539dface1
rrcampbell-exe <campbell.ryan.r@gmail.com>
2025-06-19 16:52:46 -0500

    feat: fade in landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,15 +1,31 @@
+import { useEffect, useRef } from 'react'
 import '../App.css'
 import { Link } from 'react-router'
 import { Coelbren, Awen } from '../components'
 
-const Landing = () =>
-  <div className='landing-page'>
-    <h1 className='landing-title'>
-      <Coelbren className='the'>The</Coelbren>
-      <Coelbren className='druids-den'>Druids Den</Coelbren>
-      <p className='three-rays'><Awen /></p>
-    </h1>
-    <Link to='/spooktoberfest'>Spooktoberfest 2025 {'>'}</Link>
-  </div>
+const Landing = () => {
+  const ref = useRef()
+
+  useEffect(() => {
+    const img = new window.Image()
+    img.src = '/assets/images/druids_den_sky_image.jpg'
+    img.onload = () => {
+      if (ref.current) {
+        ref.current.classList.add('image-loaded')
+      }
+    }
+  }, [])
+
+  return (
+    <div className='landing-page' ref={ref}>
+      <h1 className='landing-title'>
+        <Coelbren className='the'>The</Coelbren>
+        <Coelbren className='druids-den'>Druids Den</Coelbren>
+        <p className='three-rays'><Awen /></p>
+      </h1>
+      <Link to='/spooktoberfest'>Spooktoberfest 2025 {'>'}</Link>
+    </div>
+  )
+}
 
 export default Landing
