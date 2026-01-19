b016a7cb38888a0c552b860140991cfd928a2ff8
rrcampbell-exe <campbell.ryan.r@gmail.com>
2026-01-19 10:49:14 -0600

    feat: introduce page nav

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,5 +3,6 @@ import Leaf from './Leaf'
 import Flower from './Flower'
 import Awen from './Awen'
 import Weather from './Weather'
+import PageNav from './PageNav'
 
-export { Coelbren, Leaf, Flower, Awen, Weather }
+export { Coelbren, Leaf, Flower, Awen, Weather, PageNav }
